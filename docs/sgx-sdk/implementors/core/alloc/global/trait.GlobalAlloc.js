62509cd2e7a05a881c5437a6a62d9ec55879bfce
Mingshen Sun <bob@mssun.me>
2020-06-01 15:09:04 -0700

    Update documentation

---
+++ docs/sgx-sdk/implementors/core/alloc/global/trait.GlobalAlloc.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["sgx_alloc"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/alloc/global/trait.GlobalAlloc.html\" title=\"trait core::alloc::global::GlobalAlloc\">GlobalAlloc</a> for <a class=\"struct\" href=\"sgx_alloc/struct.System.html\" title=\"struct sgx_alloc::System\">System</a>","synthetic":false,"types":["sgx_alloc::system::System"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
